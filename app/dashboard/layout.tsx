70f22bdb524e94eb6a529f02e8fd674993caf48a
Gilberto Jr <104727935+ggaj17@users.noreply.github.com>
2026-03-23 16:43:33 -0300

    fazendo alguns comentarios

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,3 +1,4 @@
+//o layout vai ser o o ninho para todas as outras paginas presentes na pasta.
 import SideNav from '@/app/ui/dashboard/sidenav';
  
 export default function Layout({ children }: { children: React.ReactNode }) {
@@ -6,7 +7,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <div className="w-full flex-none md:w-64">
         <SideNav />
       </div>
-      <div className="grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+      <div className="grow p-6 md:overflow-y-auto md:p-12">{children} </div>
+      {/* o children é a página que foi selecionada no sidebar */}
     </div>
   );
 }
